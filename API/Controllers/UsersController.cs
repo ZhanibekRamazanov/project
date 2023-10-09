8990f9ed82c8b517ae820daee1e2873a35099035
ZhanibekRamazanov <zhanibek512@gmail.com>
2023-10-09 15:43:33 +0600

    test 4

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,18 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")] // api/users
-public class UsersController: ControllerBase
+
+[Authorize]
+
+public class UsersController: BaseApiController
 {
+    
     private readonly DataContext _context;
 
     public UsersController(DataContext context)
     {
         _context = context;
     }
-
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
@@ -21,7 +24,7 @@ public class UsersController: ControllerBase
         return users;
     }
 
-    [HttpGet("{id}")] // api/users/2
+    [HttpGet("{id}")] 
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
         return await _context.Users.FindAsync(id);
